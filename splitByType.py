a06b59ec80bc5278bf75414b4a243ffca49c91fe
Andrew Giel <agiel@stanford.edu>
2013-11-10 15:27:06 -0800

    Added Stanford Dataset

--- splitByType.py
+++ splitByType.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 FILENAME = "yelp_academic_dataset.json"
 USER = "user"
@@ -9,24 +10,30 @@ REVIEW_FILE = "review.json"
 BUSINESS_FILE = "business.json"
 
 def writeToFile(filename, objs):
-	toWriteFile = open(filename)
+	toWriteFile = open(filename, "w")
 	for obj in objs:
 		toWriteFile.write(obj + "\n")
 	toWriteFile.close()
 
+
+# os.remove(USER_FILE)
+# os.remove(REVIEW_FILE)
+# os.remove(BUSINESS_FILE)
 users = set()
 reviews = set()
 businesses = set()
 dataset = open(FILENAME)
+print "running through data"
 for line in dataset:
-	obj = dict(line)
+	obj = json.loads(line)
 	if obj['type'] == USER:
 		users.add(json.dumps(obj))
 	elif obj['type'] == REVIEW:
 		reviews.add(json.dumps(obj))
-	else obj['type'] == BUSINESS:
+	elif obj['type'] == BUSINESS:
 		businesses.add(json.dumps(obj))
 
+print "writing data"
 writeToFile(USER_FILE, users)
 writeToFile(REVIEW_FILE, reviews)
 writeToFile(BUSINESS_FILE, businesses)
