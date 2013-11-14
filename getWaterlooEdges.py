e2cf3fd686a1038cd19fc23630145b0ca2dd5be5
Siddharth Jain <siddharthjain88@gmail.com>
2013-11-13 22:43:45 -0800

    added waterloo

---
+++ getWaterlooEdges.py
@@ -0,0 +1,30 @@
+import json 
+
+REVIEW_FILE = "review.json"
+BUSINESS_FILE = "business.json"
+STANFORD_FILE = "waterlooEdges.txt"
+
+stanfordBusinesses = set()
+stanfordReviews = set()
+
+businessFile = open(BUSINESS_FILE)
+for line in businessFile:
+	business = json.loads(line)
+	if "University of Waterloo" in business['schools']: 
+		stanfordBusinesses.add("bb" + business['business_id'])
+
+reviewsFile = open(REVIEW_FILE)
+for line in reviewsFile:
+	review = json.loads(line)
+	businessID = "bb" + review['business_id'] 
+	if businessID in stanfordBusinesses:
+		reviewerID = "uu" + review['user_id']
+		stars = str(review['stars'])
+		date = review['date']
+		toAdd = ",".join([reviewerID, businessID, stars, date])
+		stanfordReviews.add(toAdd)
+
+stanfordFile = open(STANFORD_FILE, "w")
+for sR in stanfordReviews:
+	stanfordFile.write(sR + "\n")
+stanfordFile.close()
