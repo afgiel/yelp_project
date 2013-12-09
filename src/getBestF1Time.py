e1c957f672b7e5d0024d4ebd981383ba3acdd060
Andrew Giel <agiel@stanford.edu>
2013-12-09 03:57:53 -0800

    moved stuff around, also calculated best for month and year

--- src/getBestF1Time.py
+++ src/getBestF1Time.py
@@ -1,10 +1,10 @@
 
-
-FIFTY_SPLIT_THRESHOLD_PATH = "../data/threshold_training/"
+MONTHS_THRESHOLD_PATH = "../data/months/threshold_training/cumulative/"
+YEARS_THRESHOLD_PATH = "../data/years/threshold_training/"
 
 WEIGHTS_ACR = ["SW", "JW", "DW", "RSW", "RJW", "RDW", "RLSW"]
 
-SCHOOL_ACR = ["COR", "PRC", "UMC", "UIC", "CMU"]
+SCHOOL_ACR = ["STA"]
 
 def getF1(precision, recall):
 	numer = 2 * precision * recall
@@ -12,13 +12,13 @@ def getF1(precision, recall):
 	return numer/denom if denom != 0 else -1.0
 
 def getBestForWeight(weightAcr, schoolAcr):
-	resultFile = open(FIFTY_SPLIT_THRESHOLD_PATH + schoolAcr + "_" + weightAcr + "_" + "thresh_train.txt")
+	resultFile = open(YEARS_THRESHOLD_PATH + schoolAcr + "_" + weightAcr + "_" + "TRAINING.txt")
 	highF1 = 0.0
 	highPrecision = 0.0
 	highRecall = 0.0
 	choiceThreshold = None
 	for line in resultFile:
-		tau, T, precision, recall = line.split(",")
+		tau, precision, recall = line[1:len(line)-2].split(",")
 		if precision != "-1.0" and recall != "-1.0":			
 			F1 = getF1(float(precision), float(recall))
 			if F1 > highF1 or choiceThreshold is None:
@@ -30,7 +30,7 @@ def getBestForWeight(weightAcr, schoolAcr):
 
 
 def writeToFileForSchool(results, schoolAcr):
-	fileToWrite = open(FIFTY_SPLIT_THRESHOLD_PATH + "BEST_" + schoolAcr + ".txt", "w+")
+	fileToWrite = open(YEARS_THRESHOLD_PATH + "BEST_" + schoolAcr + ".txt", "w+")
 	fileToWrite.write("Weight, Tau, F1, P, R\n")
 	for line in results:
 		toWrite = str(line[0]) + "," + str(line[1]) + "," + str(line[2]) + "," + str(line[3]) + "," + str(line[4]) + "\n"
