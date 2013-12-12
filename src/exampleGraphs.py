8b3e55d544e1eb8c44d90c4655fd3e354843e818
Andrew Giel <agiel@stanford.edu>
2013-12-12 10:02:41 -0800

    added internal link example graph

--- src/exampleGraphs.py
+++ src/exampleGraphs.py
@@ -8,21 +8,17 @@ G = nx.Graph()
 G.add_nodes_from(B)
 G.add_nodes_from(U)
 
-G.add_edge(U[0], B[0])
-G.add_edge(U[0], B[1])
+G.add_edge(U[0], B[2])
+G.add_edge(U[0], B[3])
+G.add_edge(U[0], B[4])
 
-G.add_edge(U[1], B[0])
-G.add_edge(U[1], B[2])
-G.add_edge(U[1], B[4])
+G.add_edge(U[1], B[1])
+G.add_edge(U[1], B[3])
 
-G.add_edge(U[2], B[0])
-G.add_edge(U[2], B[2])
-G.add_edge(U[2], B[4])
+G.add_edge(U[2], B[1])
+G.add_edge(U[2], B[3])
 
-G.add_edge(U[3], B[1])
-G.add_edge(U[3], B[2])
-G.add_edge(U[3], B[3])
-G.add_edge(U[3], B[4])
+G.add_edge(U[3], B[0])
 
 pos = dict()
 for i in range(len(B)):
